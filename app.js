624552ae620c6ee6e534072c3595aac2bfb93cc5
skinerman <40955376+skinerman@users.noreply.github.com>
2024-09-04 01:07:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Gena'))
+app.get('/', (req, res) => res.send('Dosvidania'))
 app.listen(3000, () => console.log('Server ready'))
